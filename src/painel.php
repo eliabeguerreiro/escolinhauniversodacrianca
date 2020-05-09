b23c27054002e646af232052741e0ff5a3bf8e02
Eliabe  Paz <eliabepaz.work@gmail.com>
2020-05-09 14:56:30 -0300

    design do paínel

--- src/painel.php
+++ src/painel.php
@@ -1,19 +1,67 @@
 <?php
 session_start();
-include("conexao.php");
-if(isset($_SESSION['msg'])){
-	echo $_SESSION['msg'];
-	unset($_SESSION['msg']);
-};
-if(!empty($_SESSION['id'])){
-	echo $_SESSION['acesso'].' '.$_SESSION['nome'].", Bem vindo!<br>";
-	echo "<a href='sair.php'>Sair</a></br>";
-}else{
-	$_SESSION['msg'] = "Login expirou!";
-	header("Location: ../index.php");
-};
-
 $matricula[] = str_split($_SESSION['matricula']);
+?>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Login</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="style-salas.css">
+	</head>
+	<body>
+		<?php
+			if(!empty($_SESSION['id'])){
+				
+			}else{
+				$_SESSION['msg'] = "Login expirou!";
+				header("Location: ../index.php");
+			}
+			if(isset($_SESSION['msg'])){
+				echo "<div class='alert alert-danger' role='alert'>";
+				echo$_SESSION['msg'];
+				echo"</div>";
+				unset($_SESSION['msg']);
+			}
+			if(isset($_SESSION['msgcad'])){
+				echo "<div class='alert alert-danger' role='alert'>";
+				echo$_SESSION['msgcad'];
+				echo"</div>";
+				unset($_SESSION['msg']);
+			}?>
+		<div class="upper-content">
+            <nav>
+                <h3 id='sala'>Sala do 3°ano</h3>
+                <h3 id='nome'><?php echo $_SESSION['acesso'].' '.$_SESSION['nome']."<br>";?></h3> 
+                <a href='sair.php' id='sair'><img src="img/logout.png"></img></a>               
+			</nav>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 echo '<a href="1ano.php">1°Ano</a></br>';
